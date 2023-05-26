c60fc3fbe15b4c7e7f4faae4a411328d9df85ce8
graciellenascimento <grazciellenascimento@gmail.com>
2023-05-26 16:37:24 -0300

    final arrangements

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "photoCover":"https://image.winudf.com/v2/image/Y29tLmFuZHJvbW8uZGV2MjY2Mjk5LmFwcDUxNTU3N19zY3JlZW5zaG90c180X2E1NDNiMzI3/screen-4.webp?fakeurl=1&type=.webp",
+        "title":"gato feliz",
+        "description":"hahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahaha"
+    },
+    {
+        "id":"2",
+        "photoCover":"https://i0.wp.com/profissaobiotec.com.br/wp-content/uploads/2020/01/cat-4262034_1920.jpg?fit=1088%2C725&ssl=1",
+        "title":"gato tigrinho",
+        "description":"descriçao"
+    }
+]
