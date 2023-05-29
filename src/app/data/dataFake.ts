fc631f5c4054e0b0d218fd5c8798b8027ab02f57
graciellenascimento <grazciellenascimento@gmail.com>
2023-05-29 20:44:51 -0300

    adjustments

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -2,13 +2,25 @@ export const dataFake = [
     {
         "id":"1",
         "photoCover":"https://image.winudf.com/v2/image/Y29tLmFuZHJvbW8uZGV2MjY2Mjk5LmFwcDUxNTU3N19zY3JlZW5zaG90c180X2E1NDNiMzI3/screen-4.webp?fakeurl=1&type=.webp",
-        "title":"gato feliz",
-        "description":"hahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahahaha"
+        "title":"Tudo sobre gato: 10 dicas de saúde para seu pet",
+        "description":"https://www.patinhascarentes.org/post/10-dicas-para-cuidar-melhor-do-seu-gato"
     },
     {
         "id":"2",
-        "photoCover":"https://i0.wp.com/profissaobiotec.com.br/wp-content/uploads/2020/01/cat-4262034_1920.jpg?fit=1088%2C725&ssl=1",
-        "title":"gato tigrinho",
-        "description":"descriçao"
+        "photoCover":"https://image.winudf.com/v2/image/Y29tLmFuZHJvbW8uZGV2MjY2Mjk5LmFwcDUxNTU3N19zY3JlZW5zaG90c180X2E1NDNiMzI3/screen-4.webp?fakeurl=1&type=.webp",
+        "title":"9 maneiras de deixar seu gato feliz",
+        "description":"https://blog.cobasi.com.br/gato-feliz/"
+    },
+    {
+        "id":"3",
+        "photoCover":"https://image.winudf.com/v2/image/Y29tLmFuZHJvbW8uZGV2MjY2Mjk5LmFwcDUxNTU3N19zY3JlZW5zaG90c180X2E1NDNiMzI3/screen-4.webp?fakeurl=1&type=.webp",
+        "title":"5 acessórios indispensáveis para a rotina do felino",
+        "description":"https://formulanimal.com.br/filhote-de-gato-principais-cuidados/"
+    },
+    {
+        "id":"4",
+        "photoCover":"https://image.winudf.com/v2/image/Y29tLmFuZHJvbW8uZGV2MjY2Mjk5LmFwcDUxNTU3N19zY3JlZW5zaG90c180X2E1NDNiMzI3/screen-4.webp?fakeurl=1&type=.webp",
+        "title":"O que o gato gosta de comer além da ração normal?",
+        "description":"https://blog.cobasi.com.br/o-que-dar-para-gato-comer-quando-nao-tem-racao/"
     }
 ]
